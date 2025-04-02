291aa1ce5acbe326f61adb3fc0a8bcec9d5364d6
Juan Badone <juansebastianbadone_@hotmail.com>
2025-04-02 12:56:50 -0300

    agregando animaciones

--- js/login/login.js
+++ js/login/login.js
@@ -2,20 +2,20 @@ function showLogin() {
     document.getElementById("loginForm").style.display = "flex";
     document.getElementById("registerForm").style.display = "none";
     
-    document.getElementById("loginBtn").classList.add("active");
-    document.getElementById("loginBtn").classList.remove("inactive");
+    document.getElementById("loginBtn").classList.add("active_l");
+    document.getElementById("loginBtn").classList.remove("inactive_l");
 
-    document.getElementById("registerBtn").classList.add("inactive");
-    document.getElementById("registerBtn").classList.remove("active");
+    document.getElementById("registerBtn").classList.add("inactive_l");
+    document.getElementById("registerBtn").classList.remove("active_l");
 }
 
 function showRegister() {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("registerForm").style.display = "flex";
 
-    document.getElementById("registerBtn").classList.add("active");
-    document.getElementById("registerBtn").classList.remove("inactive");
+    document.getElementById("registerBtn").classList.add("active_l");
+    document.getElementById("registerBtn").classList.remove("inactive_l");
 
-    document.getElementById("loginBtn").classList.add("inactive");
-    document.getElementById("loginBtn").classList.remove("active");
+    document.getElementById("loginBtn").classList.add("inactive_l");
+    document.getElementById("loginBtn").classList.remove("active_l");
 }
